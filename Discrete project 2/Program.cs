579c6f3c413c37dfd11a48089fd52fa7022e18cd
Ebrahim Zaher <112896816+ebrahimzaher@users.noreply.github.com>
2022-12-24 14:54:32 +0200

    Add files via upload

---
+++ Discrete project 2/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace Discrete_project_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter your number");
+            
+            long n =long.Parse(Console.ReadLine());
+
+            long sum = 0;
+
+            for(int i = 1; i <= n/2 ; i++)
+            {
+
+                if (n % i == 0)
+                {
+
+                    sum += i;
+                    
+                }
+            
+            }
+
+            if (sum == n)
+            {
+
+                Console.WriteLine(n + " is perfect");
+            
+            }
+
+            else
+            {
+            
+                Console.WriteLine(n + " is not perfect");
+            
+            }
+
+        }
+    }
+}
